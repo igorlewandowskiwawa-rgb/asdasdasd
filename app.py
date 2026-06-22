5e06d3b1cca67cca6220fc670dbf1dc7ef8211f2
igorlewandowskiwawa-rgb <igor.lewandowski.wawa@gmail.com>
2026-06-22 13:32:19 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
@@ -258,5 +259,5 @@ def login():
 
 if __name__ == '__main__':
 import os
-port = int(os.environ.get("PORT", port= 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
